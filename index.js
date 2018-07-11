746082b15655ea95abd09f5e872f46081b83443e
joanissedan@gmail.com <joanissedan@gmail.com>
2018-07-11 21:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays('') {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  name(${name1}) {
+    return "Happy holidays, `${name1}'!"
+  }
+}
